5eaaf26ec6d459e7a75e67074396802b24f776fc
biratdatta <birat.datta26@gmail.com>
2022-09-06 23:55:26 +0530

    changes

---
+++ hello.java
@@ -0,0 +1,8 @@
+//Hello world program
+public class hello
+{
+public static void main(String args[])
+{
+System.out.println("Hello World");
+}
+}
--- readme.txt
+++
@@ -1,4 +0,0 @@
-This is the Readme/ Master Branch. 
-
-Attaching here is the Action Plan
-
