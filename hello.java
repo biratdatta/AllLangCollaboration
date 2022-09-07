4d08ef110ce3eb0ec6d17a29de14862e9ca1cde6
biratdatta <birat.datta26@gmail.com>
2022-09-07 23:01:51 +0530

    changes

--- hello.java
+++ hello.java
@@ -1,12 +1,18 @@
-//Hello world program
-public class hello
+ 
+
+// Program to convert celcius to fahrenheit
+import java.util.Scanner;
+public class celcius
 {
 public static void main(String args[])
 {
-System.out.println("Hello New");
-}
-}
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the temperature in celcius");
+int c = sc.nextInt();
+int f = (c*9/5)+32;
 
+System.out.println("The temperature in fahrenheit is "+f);
+}
+}
 
 // Language: java
-// Path: hello.java
