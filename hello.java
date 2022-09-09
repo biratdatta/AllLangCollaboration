3e784aac5f607aaf63851d427739716d11427088
biratdatta <birat.datta26@gmail.com>
2022-09-09 11:49:46 +0530

    changes

--- hello.java
+++ hello.java
@@ -1,4 +1,21 @@
  
+// How to install packages/import libraries
+//A package in Java is used to group related classes. It is a way to put classes and interfaces on a logical group. 
+// Example: import package.name.Class;   // Import a single class
+// Example: import package.name.*;       // Import all classes in a package
+
+// How to declare a class
+// A class is a blueprint from which individual objects are created.
+// Example: public class MyClass {
+
+// How to declare a method
+// A method is a block of code which only runs when it is called.
+// Example: public static void myMethod() 
+
+
+
+
+
 
 // Program to convert celcius to fahrenheit
 import java.util.Scanner;
