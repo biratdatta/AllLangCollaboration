c6b206fa87233d05f8340cce77a1ff0bd43ee1f4
biratdatta <birat.datta26@gmail.com>
2022-09-10 15:15:55 +0530

    changes

--- hello.java
+++ hello.java
@@ -1,4 +1,6 @@
- 
+ // Basic Introduction to Java
+
+
 // How to install packages/import libraries
 //A package in Java is used to group related classes. It is a way to put classes and interfaces on a logical group. 
 // Example: import package.name.Class;   // Import a single class
@@ -12,12 +14,20 @@
 // A method is a block of code which only runs when it is called.
 // Example: public static void myMethod() 
 
+// How to declare a variable
+
+
+
+
+
+
 
 
 
 
 
 // Program to convert celcius to fahrenheit
+
 import java.util.Scanner;
 public class celcius
 {
@@ -31,6 +41,7 @@ int f = (c*9/5)+32;
 System.out.println("The temperature in fahrenheit is "+f);
 }
 }
+
 
 
 
